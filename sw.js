eba94953324df9d5a82d90f255ddff8ddad2f571
jalallelrumii-png <jalallelrumii@gmail.com>
2026-02-10 16:05:07 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,17 @@
-const CACHE_NAME = 'lf-store-v2';
+const CACHE_NAME = 'lf-native-v3';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
   'https://cdn.tailwindcss.com',
-  'https://unpkg.com/vue@3/dist/vue.global.js',
-  'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;600;800&display=swap'
+  'https://unpkg.com/vue@3/dist/vue.global.js'
 ];
 
-// Install Service Worker
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-// Strategi: Cek Cache dulu, kalo ga ada baru download (Offline First)
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
