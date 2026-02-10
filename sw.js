80d266e0f6fc354859b94c7d6708986e8080dfe1
jalallelrumii-png <jalallelrumii@gmail.com>
2026-02-10 15:58:04 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,28 @@
-const CACHE_NAME = 'lf-ultimate-v1';
+const CACHE_NAME = 'lf-store-v2';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
   'https://cdn.tailwindcss.com',
-  'https://unpkg.com/vue@3/dist/vue.global.js'
+  'https://unpkg.com/vue@3/dist/vue.global.js',
+  'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;600;800&display=swap'
 ];
 
-// Tahap Install: Simpan semua file ke memori HP
+// Install Service Worker
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
   self.skipWaiting();
 });
 
-// Tahap Fetch: Kalo internet mati, ambil dari memori (Cache)
+// Strategi: Cek Cache dulu, kalo ga ada baru download (Offline First)
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
   );
 });
